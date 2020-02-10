7b099fdee4b9eebdb6e256edccac71a112e332d3
ShaunFerns <shaun.ferns@tudublin.ie>
2020-02-10 12:34:01 +0000

    ist commit

---
+++ Assets/_Scenes/_Scripts/SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadOnClick(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
